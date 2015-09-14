016494401de2c6f9d7fcca4bbaea3ded7b0ed4e8
Murali Vilayannur <mnv104@gmail.com>
2015-09-14 08:54:48 -0700

    Utility to print if a sequence is sorted in ascending or descending order.

--- isSorted.cpp
+++ isSorted.cpp
@@ -1,4 +1,10 @@
 #include <vector>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <getopt.h>
+#include <stdlib.h>
 using namespace std;
 
 
@@ -18,4 +24,34 @@ bool isSorted(const std::vector<int> nums)
       }
    }
    return sorted;
+}
+
+
+int main(int argc, char *argv[])
+{
+   std::vector<int> v;
+   int c;
+   while ((c = getopt(argc, argv, "v:")) != EOF) {
+      switch (c) {
+         case 'v':
+            {
+               std::stringstream ss(optarg);
+               std::string token;
+               while (std::getline(ss, token, ',')) {
+                  std::string::size_type sz;
+                  int a = std::stoi(token, &sz);
+                  v.push_back(a);
+               }
+               break;
+            }
+         default:
+            exit(1);
+      }
+   }
+   if (v.size() == 0) {
+      cout << "Usage: " << argv[0] << " -v {list of comma separated numbers}" << endl;
+      exit(1);
+   }
+   cout << isSorted(v) << endl;
+   return 0;
 }
